61b186c9b57543721fb41021a82daa538956e682
SeagullOO <3164220387@qq.com>
2025-09-11 15:00:08 +0800

    笔记本上关于about的修改

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/10/hello-world/"];function toRandomPost(){
+var posts=["2025/09/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
