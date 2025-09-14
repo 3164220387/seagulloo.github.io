c95ef725f2dd5b446d7c0b9bcc5c8f2c6b0c9207
SeagullOO <3164220387@qq.com>
2025-09-14 12:34:43 +0800

    Site updated: 2025-09-14 12:34:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/12/Hexo使用笔记/","2025/09/13/安知鱼主题自定义教程/"];function toRandomPost(){
+var posts=["2025/09/13/安知鱼主题自定义教程/","2025/09/12/Hexo+Github搭建个人网站笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
